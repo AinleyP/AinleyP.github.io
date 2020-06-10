777e9e37bf2056632ba37a65025fea294e54590f
ainleyp <ainleypena@gmail.com>
2020-06-10 15:43:03 -0400

    changing build to docs

---
+++ docs/precache-manifest.f6df84b6b07e462a22e17bd118b64d97.js
@@ -0,0 +1,214 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ebb89971892ed1cf8f3a2adbb5dedd88",
+    "url": "/ainleyp.github.io/index.html"
+  },
+  {
+    "revision": "9c707214a0f16d4fd85d",
+    "url": "/ainleyp.github.io/static/css/2.4707e12a.chunk.css"
+  },
+  {
+    "revision": "73a8d7383656ae013504",
+    "url": "/ainleyp.github.io/static/css/main.24977a13.chunk.css"
+  },
+  {
+    "revision": "9c707214a0f16d4fd85d",
+    "url": "/ainleyp.github.io/static/js/2.d581bb55.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/ainleyp.github.io/static/js/2.d581bb55.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "73a8d7383656ae013504",
+    "url": "/ainleyp.github.io/static/js/main.305b7b96.chunk.js"
+  },
+  {
+    "revision": "c96f6b16d9ec2e48a4ee",
+    "url": "/ainleyp.github.io/static/js/runtime-main.1ca94290.js"
+  },
+  {
+    "revision": "daff26c0a9b5b824d736a29bee7f2a79",
+    "url": "/ainleyp.github.io/static/media/Sitemap.daff26c0.png"
+  },
+  {
+    "revision": "4e72fbec3ae2e932e049e5da3dee70e3",
+    "url": "/ainleyp.github.io/static/media/ap-header.4e72fbec.png"
+  },
+  {
+    "revision": "635142faada3d56c99782a9f90f4b5cc",
+    "url": "/ainleyp.github.io/static/media/aws-mockup.635142fa.png"
+  },
+  {
+    "revision": "84e5de48f3ac90b85ae94979706381b9",
+    "url": "/ainleyp.github.io/static/media/blueprint_logo.84e5de48.png"
+  },
+  {
+    "revision": "2867c045cd6049800d24039990de2b75",
+    "url": "/ainleyp.github.io/static/media/branch-demo.2867c045.JPG"
+  },
+  {
+    "revision": "082b291662e763b97fd53d99571e4732",
+    "url": "/ainleyp.github.io/static/media/dm-sans-v4-latin-700.082b2916.woff"
+  },
+  {
+    "revision": "171ccc885c8deb88a0f3b55096584514",
+    "url": "/ainleyp.github.io/static/media/dm-sans-v4-latin-700.171ccc88.woff2"
+  },
+  {
+    "revision": "279b8d594e2da9085e15bd6f46359f32",
+    "url": "/ainleyp.github.io/static/media/dm-sans-v4-latin-700.279b8d59.ttf"
+  },
+  {
+    "revision": "8c3fecb43ad57f658ef91a121f452626",
+    "url": "/ainleyp.github.io/static/media/dm-sans-v4-latin-700.8c3fecb4.eot"
+  },
+  {
+    "revision": "b4f432fbedfd673a9793a780102b16b8",
+    "url": "/ainleyp.github.io/static/media/dm-sans-v4-latin-700.b4f432fb.svg"
+  },
+  {
+    "revision": "30c783f39bfdcb075cc20bec7f6124ab",
+    "url": "/ainleyp.github.io/static/media/dm-sans-v4-latin-regular.30c783f3.ttf"
+  },
+  {
+    "revision": "4322946874996ad9a91ca8ccbd3a0e19",
+    "url": "/ainleyp.github.io/static/media/dm-sans-v4-latin-regular.43229468.svg"
+  },
+  {
+    "revision": "54706a9e804757f8d8edea7d7e9f2577",
+    "url": "/ainleyp.github.io/static/media/dm-sans-v4-latin-regular.54706a9e.woff"
+  },
+  {
+    "revision": "77ce2417018768bd7a8297497f75e259",
+    "url": "/ainleyp.github.io/static/media/dm-sans-v4-latin-regular.77ce2417.eot"
+  },
+  {
+    "revision": "ad513518b8de790106aefb37f7304835",
+    "url": "/ainleyp.github.io/static/media/dm-sans-v4-latin-regular.ad513518.woff2"
+  },
+  {
+    "revision": "1c546cc508ee7325f4f91f046321aa61",
+    "url": "/ainleyp.github.io/static/media/dominican-trip.1c546cc5.png"
+  },
+  {
+    "revision": "770035886fa79679408e75c0927a5c08",
+    "url": "/ainleyp.github.io/static/media/fassb-design-process.77003588.png"
+  },
+  {
+    "revision": "b259d290aef621b055a17b7bfe092764",
+    "url": "/ainleyp.github.io/static/media/fassb-header.b259d290.png"
+  },
+  {
+    "revision": "57cc9e5473ff70f7a4fcbd2090b4912e",
+    "url": "/ainleyp.github.io/static/media/fassb-mockup.57cc9e54.png"
+  },
+  {
+    "revision": "2b064e13ea42cd42b41d83358208d736",
+    "url": "/ainleyp.github.io/static/media/fassbsurvey-1.2b064e13.PNG"
+  },
+  {
+    "revision": "3ed69e99e2bbc2c46d3dd92d43149c17",
+    "url": "/ainleyp.github.io/static/media/figureman-1.3ed69e99.png"
+  },
+  {
+    "revision": "84793b108191bd048f3633f9ca7fd5c5",
+    "url": "/ainleyp.github.io/static/media/figureman-2.84793b10.png"
+  },
+  {
+    "revision": "2c45c03b72eaccaf225006253bb44d7c",
+    "url": "/ainleyp.github.io/static/media/figureman-3.2c45c03b.png"
+  },
+  {
+    "revision": "459e6427d5069fc6db354c42d7585352",
+    "url": "/ainleyp.github.io/static/media/figuretext-1.459e6427.png"
+  },
+  {
+    "revision": "70ad4c0275ff24a32bc96d2355f7830b",
+    "url": "/ainleyp.github.io/static/media/figuretext-2.70ad4c02.png"
+  },
+  {
+    "revision": "ee04bd18d0dca89d7aec38d23f6e2fee",
+    "url": "/ainleyp.github.io/static/media/figuretext-3.ee04bd18.png"
+  },
+  {
+    "revision": "9f6f6cc167573b4e08861f9f2d673e6e",
+    "url": "/ainleyp.github.io/static/media/koinonia-mockup.9f6f6cc1.png"
+  },
+  {
+    "revision": "0ed62943660c278d396568fb6d09dbe2",
+    "url": "/ainleyp.github.io/static/media/lp-1.0ed62943.png"
+  },
+  {
+    "revision": "b56bc15dd7f5875a58cb64b249687d09",
+    "url": "/ainleyp.github.io/static/media/lp-2.b56bc15d.png"
+  },
+  {
+    "revision": "a88d739adbc3b6c741445b4bbe638505",
+    "url": "/ainleyp.github.io/static/media/lp-3.a88d739a.png"
+  },
+  {
+    "revision": "e3830e0fb8e1a4a503992849e578cceb",
+    "url": "/ainleyp.github.io/static/media/lp-4.e3830e0f.png"
+  },
+  {
+    "revision": "8fddd126bce41d7d03ea6fa7fa0a828e",
+    "url": "/ainleyp.github.io/static/media/lp-6.8fddd126.png"
+  },
+  {
+    "revision": "4514925f9fd71bbbb347ee44c3a93cd6",
+    "url": "/ainleyp.github.io/static/media/lp-final.4514925f.png"
+  },
+  {
+    "revision": "7f5841e9a15274e01b40cc10ce4a4cc3",
+    "url": "/ainleyp.github.io/static/media/noteify-mockup.7f5841e9.png"
+  },
+  {
+    "revision": "5dfabc24147ae633c864fb980a94ea7c",
+    "url": "/ainleyp.github.io/static/media/persona-1.5dfabc24.png"
+  },
+  {
+    "revision": "de18793502314f7bc764c820a9f2c5db",
+    "url": "/ainleyp.github.io/static/media/persona-2.de187935.png"
+  },
+  {
+    "revision": "1daa50c8acdb0fd5bed6fbae15915203",
+    "url": "/ainleyp.github.io/static/media/persona-3.1daa50c8.png"
+  },
+  {
+    "revision": "67aef732e9c9772eb2a8ea7597a962b1",
+    "url": "/ainleyp.github.io/static/media/postit-activity.67aef732.JPG"
+  },
+  {
+    "revision": "8bef4c47bc66119054bd023a20bd7b92",
+    "url": "/ainleyp.github.io/static/media/prototype-2.8bef4c47.PNG"
+  },
+  {
+    "revision": "f3472028afe4ae1e0ad00b2c2731d35b",
+    "url": "/ainleyp.github.io/static/media/sdc-header.f3472028.png"
+  },
+  {
+    "revision": "218d872bc9d4590ceed12f3393f59f1f",
+    "url": "/ainleyp.github.io/static/media/sdc-mockup.218d872b.png"
+  },
+  {
+    "revision": "ac88fd81f01d9c4a9ef6936bab7ccf67",
+    "url": "/ainleyp.github.io/static/media/sdc-old-flowchart-1.ac88fd81.png"
+  },
+  {
+    "revision": "59d615067a32fbaaefeb7d714a0aff1e",
+    "url": "/ainleyp.github.io/static/media/spoken-word.59d61506.png"
+  },
+  {
+    "revision": "0585a1535d34172245f21421dce40086",
+    "url": "/ainleyp.github.io/static/media/ulti-frisbee.0585a153.png"
+  },
+  {
+    "revision": "e0421393683f64b7c21387d0076dec09",
+    "url": "/ainleyp.github.io/static/media/yellowblob1.e0421393.png"
+  },
+  {
+    "revision": "cc35bc6630e2eee7b5cf4c243cc778a0",
+    "url": "/ainleyp.github.io/static/media/yellowblob2.cc35bc66.png"
+  }
+]);
